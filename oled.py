47254aa15bf50934245924cbc7fe7256092c5033
eugenMaier <92370549+eugenMaier@users.noreply.github.com>
2021-12-07 11:30:23 +0100

    OLED mit Raumfeuchtigkeit

--- oled.py
+++ oled.py
@@ -1,43 +1,75 @@
 
 
-from time import sleep                                  #aus Uhren funktionen wirt sleep importiert
-from machine import Pin, SoftSPI, SoftI2C               #es werden die Pins und die Ansteuerungs Buse von dem Chip abgelsen
-import st7789py as st7789                               #Bibliothek st7789py wird zu st7789 umgewandelt
-from bmp180 import BMP180                               #aus der Bibliothek bmp180 wird die Klasse BMP180 importiert
+from time import sleep                                  # alle 10 Sekunden die Temperatur messen
+from machine import Pin, SoftSPI, SoftI2C               # Pin , SoftSPI (TFT), SoftI2C (BMP180)
+import st7789py as st7789                               # TFT Display
+from bmp180 import BMP180                               # BMP180 Temperatursensor
+from bh1750 import BH1750                               # BH1750 Lichtsensor
+from htu2x import HTU21D
 
 #choose fonts
 
-from romfonts import vga2_16x16 as font                 #Schirftart wird ausgewählt
+from romfonts import vga2_16x16 as font                 # Schriftart laden
 
 
-i2c = SoftI2C (scl=Pin(22), sda=Pin(21))                #wird angegeben unter welchen Pins der I2C Bus angesteuert wird
+i2c = SoftI2C (scl=Pin(22), sda=Pin(21))                # I2C (BMP180)
 
-bmp = BMP180(i2c)                                       #Objekt bmp wird werstellt und BMP180 wird i2c zugewiesen
+bmp = BMP180(i2c)                                       # Objekt bmp instanzieren
+bh = BH1750(i2c)                                        # Objekt bh instanzieren
+ht = HTU21D(22,21)
 
+ledgruen = Pin(25, Pin.OUT)                             # Objekt ledgruen instanzieren
+ledgelb = Pin(26, Pin.OUT)                              # Objekt ledgelb instanzieren
+ledrot = Pin(27, Pin.OUT)                               # Objekt ledrot instanzieren
 
-spi = SoftSPI(                                          #Variablen für den SoftSPI Bus werden festgelegt
-    baudrate=20000000,
-    polarity=1,
+spi = SoftSPI(                                          # Objekt spi instanzieren
+    baudrate=20000000,                                  # Kummunikationsgeschwindigkeit
+    polarity=1,                                         
     phase=0,
     sck=Pin(18),
     mosi=Pin(19),
     miso=Pin(13))
 
-tft = st7789.ST7789(                                    #Variablen für den Display werden festgelegt
-    spi,
-    135,
-    240,
+tft = st7789.ST7789(                                    # Objekt tft instanzieren
+    spi,                                                # Schnittstelle
+    135,                                                # Pixel x-Achse (hochkant)
+    240,                                                # Pixel y-Achse
     reset=Pin(23, Pin.OUT),
     cs=Pin(5, Pin.OUT),
     dc=Pin(16, Pin.OUT),
     backlight=Pin(4, Pin.OUT),
-    rotation=0)
+    rotation=1)                                         # Querformat
 
-#tft.vscrdef(40,240,40)
 
-tft.fill(st7789.GREEN)                                  #Hintergrundfarbe
-line = 0                                                #Anfangsparameter
-col = 0                                                 #Anfangsparameter
-while True:                                             #Schleife wird eingebaut
-    ausgabe = str(bmp.temperature)                      #Objekt asugabe wird deklariert und wir als String ausgegeben
-    tft.text(font, ausgabe, 10, 10, st7789.BLUE, st7789.YELLOW) #Text ausgabe 
+#----------------------------Initialisierung Ende-------------------------------------------------
+
+tft.fill(st7789.BLACK)                                  # Hintergrundfarbe Grün
+line = 0                                                # Textbeginn Zeile
+col = 0                                                 # Textbeginn Spalte
+while True:
+
+    #led nach Temperatur anzeigen
+    if bmp.temperature <= 24:                           
+        ledgruen.value(1)
+        ledgelb.value(0)
+        ledrot.value(0)
+    elif bmp.temperature >25:
+        ledrot.value(1)
+        ledgelb.value(0)
+        ledgruen.value(0)
+    else:
+        ledgelb.value(1)
+        ledrot.value(0)
+        ledgruen.value(0) 
+    # Text ausgabe                                        
+    temp = str(round(bmp.temperature,1))             # Objekt ausgabe wird deklariert und wird als String ausgegeben
+    druck = str(round(bmp.pressure/100,1))           # Objekt druck initialisieren
+    licht = str(round(bh.luminance(BH1750.CONT_HIRES_1)))
+    luft = str(round(ht.humidity))
+    temperatur = str(round(ht.temperature))
+    tft.text(font,'Temp:'+temp+'\xf8c', 10, 10, st7789.GREEN, st7789.BLACK)
+    tft.text(font,'L:'+druck+'hPa', 10, 30, st7789.GREEN, st7789.BLACK)       
+    tft.text(font,'Licht:'+licht+'lux', 10, 50, st7789.GREEN, st7789.BLACK)
+    tft.text(font,'Luft:'+luft+'%', 10, 70, st7789.GREEN, st7789.BLACK)
+    tft.text(font,'Temp:'+temperatur+'\xf8c', 10, 90, st7789.GREEN, st7789.BLACK)
+    sleep(1)
